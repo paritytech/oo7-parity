16c3780a87a69da6f742fe8c48aef0a2d07ea55a
Jaco Greeff <jaco@parity.io>
2017-09-26 13:13:29 +0200

    Add licence headers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+// (C) Copyright 2016-2017 Parity Technologies (UK) Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//         http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 /* global parity */
 /* eslint-disable no-return-assign */
 /* eslint-disable no-proto */
@@ -5,9 +19,9 @@
 const BigNumber = require('bignumber.js');
 const oo7 = require('oo7');
 
-const { abiPolyfill, RegistryABI, RegistryExtras, GitHubHintABI, OperationsABI, BadgeRegABI, TokenRegABI, BadgeABI, TokenABI } = require('./abis.js');
-
 const Parity = require('@parity/parity.js');
+
+const { abiPolyfill, RegistryABI, RegistryExtras, GitHubHintABI, OperationsABI, BadgeRegABI, TokenRegABI, BadgeABI, TokenABI } = require('./abis');
 
 // DEPRECATED. TODO: REMOVE
 export function setupBonds (_api = parity.api) {
